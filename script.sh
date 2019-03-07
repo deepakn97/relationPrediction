06b10f67a1dc48e9df3335204754a1e290606a1e
nomnom <deepak54354@gmail.com>
2019-03-06 19:50:38 -0800

    Added script file

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir checkpoints
+
+mkdir checkpoints/wn
+mkdir checkpoints/fb
+mkdir checkpoints/kinship
+mkdir checkpoints/umls
+mkdir checkpoints/nell
+
+mkdir checkpoints/wn/out
+mkdir checkpoints/fb/out
+mkdir checkpoints/kinship/out
+mkdir checkpoints/umls/out
+mkdir checkpoints/nell/out
